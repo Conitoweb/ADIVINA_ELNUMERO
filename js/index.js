e7ebe0357fed2e631a33706c1bb4311be88a25e5
danielbenitez <daniel29052003@gmail.com>
2024-06-28 15:01:32 -0600

    contador de vidas arreglado

--- js/index.js
+++ js/index.js
@@ -1,51 +1,52 @@
 // variables
 let vidas = 3
-let numeroAleatorio = Math.floor(Math.random()  * 11);
+let numeroAleatorio = Math.floor(Math.random() * 11);
 
 // selectors
- document.querySelector('.contador__vida').textContent = `vidas : ${vidas}`
- document.querySelector('.play > button').addEventListener("click" , play)
- const incognito =document.querySelector('.incognito')
- const descripcion = document.querySelector(".descripcion")
- const numeros = document.querySelectorAll('.numeros > button')
+document.querySelector('.contador__vida').textContent = `vidas : ${vidas}`
+document.querySelector('.play > button').addEventListener("click", play)
+const incognito = document.querySelector('.incognito')
+const descripcion = document.querySelector(".descripcion")
+const numeros = document.querySelectorAll('.numeros > button')
 
 // functions
 console.log(numeroAleatorio);
 function play() { // iniciar juego y restablecer valores 
     vidas = 3
-    numeroAleatorio = Math.floor(Math.random()  * 11)
+    numeroAleatorio = Math.floor(Math.random() * 11)
     console.log(numeroAleatorio);
-    incognito.style.backgroundColor= 'grey'
-    incognito.style.filter= "blur(3px)"
-     document.querySelector('.contador__vida').textContent = `vidas : ${vidas}`
-     incognito.innerText = " ? "
-       descripcion.innerText = "adivina el numero"
-       document.querySelector('.play').style.visibility="hidden"
-       descripcion.style.backgroundColor="white"
-       document.querySelector(".tabla__numero").style.visibility='visible'
+    incognito.style.backgroundColor = 'grey'
+    incognito.style.filter = "blur(3px)"
+    document.querySelector('.contador__vida').textContent = `vidas : ${vidas}`
+    incognito.innerText = " ? "
+    descripcion.innerText = "adivina el numero"
+    document.querySelector('.play').style.visibility = "hidden"
+    descripcion.style.backgroundColor = "white"
+    document.querySelector(".tabla__numero").style.visibility = 'visible'
     // borrar el numero mostrado en pantalla
     // mostrar la tabla de numeros
 }
-numeros.forEach( (numero) => { //recorremos los botones y buscamos su valor y lo comparamos con el numero aleatorio
-    numero.addEventListener("click", (validarNumero)=>{
+numeros.forEach((numero) => { //recorremos los botones y buscamos su valor y lo comparamos con el numero aleatorio
+    numero.addEventListener("click", (validarNumero) => {
         let valorNumero = validarNumero.target.innerText
         console.log(valorNumero);
-       if(vidas <= 0){ // aqui hacemos un control de vidas para saber si el jugador ya perdio
-        vidas++ 
-        descripcion.innerText="Perdiste :("
-        document.querySelector(".play").style.visibility= "visible"
-        document.querySelector(".tabla__numero").style.visibility='hidden'
-       }
-        if(numeroAleatorio != valorNumero){ //si no acertas que reste las vidas
-           document.querySelector('.contador__vida').textContent = `vidas :${--vidas}`
-        }else{
+        if (vidas<1&&vidas==0) { // aqui hacemos un control de vidas para saber si el jugador ya perdio
+            descripcion.innerText = "Perdiste :("
+            document.querySelector(".play").style.visibility = "visible"
+            document.querySelector(".tabla__numero").style.visibility = 'hidden'
+            vidas=0
+        }
+        else if (numeroAleatorio != valorNumero) { //si no acertas que reste las vidas
+            --vidas
+            document.querySelector('.contador__vida').textContent = `vidas :${vidas}`
+        }else if(numeroAleatorio == valorNumero) {
             console.log("acertaste!");
-            incognito.style.backgroundColor= 'green'
-            incognito.style.filter= "blur(0px)"
+            incognito.style.backgroundColor = 'green'
+            incognito.style.filter = "blur(0px)"
             incognito.innerText = numeroAleatorio
             descripcion.innerText = "!Lo Lograste!"
-            document.querySelector(".play").style.visibility= "visible"
-            
+            document.querySelector(".play").style.visibility = "visible"
+
         }
 
     })
